55f38fa5c057100c171b80a10ec8ee1f9c85cccd
ChoiKangHee <rkdgml3577@gmail.com>
2026-08-07 10:19:35 +0900

    최종실습 : 환경설정

---
+++ shopapi/src/test/java/com/sk/skala/shopapi/ShopapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.skala.shopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
